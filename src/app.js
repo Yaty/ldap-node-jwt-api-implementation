a4116cc5f36a882dc3616a9871669076a5daa939
Hugo Da Roit <contact@hdaroit.fr>
2019-02-02 13:10:06 +0100

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 const logger = require('./utils/logger')('app');
@@ -13,6 +14,7 @@ app.use(morgan('tiny', {
   },
 }));
 
+app.use(cors());
 app.use(express.static('./public'));
 app.use(bodyparser.json());
 app.use('/api', require('./routes'));
